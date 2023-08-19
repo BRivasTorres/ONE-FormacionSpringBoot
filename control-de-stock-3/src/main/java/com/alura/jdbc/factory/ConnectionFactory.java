443ca8b41282c9c22388f4a50fa989cc1fff9965
Billy Torres <btorresr75@gmail.com>
2023-08-19 07:11:32 -0600

    %1

--- control-de-stock-3/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ control-de-stock-3/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,10 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC\", \"root\", \"Hola1234.");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("Hola1234.");
+		
+		this.datasource = pooledDataSource; 
+	}
+	
 	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", "Hola1234.");
+		return this.datasource.getConnection();
 	}
 }
