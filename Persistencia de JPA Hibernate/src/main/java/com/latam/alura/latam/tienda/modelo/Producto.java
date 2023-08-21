97e9bec99632b7f5cd347655c36f76cce3b515c7
Billy Torres <btorresr75@gmail.com>
2023-08-21 17:37:55 -0600

    %1

--- Persistencia de JPA Hibernate/src/main/java/com/latam/alura/latam/tienda/modelo/Producto.java
+++ Persistencia de JPA Hibernate/src/main/java/com/latam/alura/latam/tienda/modelo/Producto.java
@@ -1,5 +1,46 @@
 package com.latam.alura.latam.tienda.modelo;
 
-public class Producto {
+import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String description;
+	private BigDecimal precio;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	
+	
 }
---
+++ Persistencia de JPA Hibernate/src/main/java/com/latam/alura/latam/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,25 @@
+package com.latam.alura.latam.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.latam.alura.latam.tienda.modelo.Producto;
+
+public class RegistroDeProducto {
+
+	public static void main(String[] args) {
+		Producto celular = new Producto();
+		celular.setNombre("Samsung");
+		celular.setDescription("telefono usado");
+		celular.setPrecio(new BigDecimal("1000"));
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda"); 
+		EntityManager em = factory.createEntityManager();
+		
+		em.persist(celular);
+	}
+
+}
